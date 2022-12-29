cafc1b07e53d7871a9c24d8b9708842c45d497ab
KlementOndrej <ondrej.klement@unob.cz>
2022-12-29 14:58:14 +0100

    Basic Snake class structure

--- src/Snake.hpp
+++ src/Snake.hpp
@@ -1,12 +1,52 @@
 #pragma once
 #include <ncurses.h>
+#include <queue>
 #include "Stuff.hpp"
+
+enum Direction{
+	up,
+	down,
+	left,
+	right
+};
 
 class SnakePiece: public Stuff{
 public:
-	
+	SnakePiece(){
+		this->x = 0;
+		this->y = 0;
+		this->icon = '#';
+	}
+
+	SnakePiece(int x, int y)[
+		this->x = x;
+		this->y = y;
+		this->icon = '#';
+	}
 };
 
 class Snake{
+public:
+	Snake(){
+		cur_dir = down;
+	}
 
+	addPiece(SnakePiece new_piece){
+		snake.push(new_piece);
+	}
+
+	removePiece(){
+		snake.pop();
+	}
+
+	SnakePiece tail(){
+		return snake.front();
+	}
+
+	SnakePiece head(){
+		return snake.back();
+	}
+private:
+	std::queue<SnakePiece> snake;
+	Direction cur_dir;
 };
