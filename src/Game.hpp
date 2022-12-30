3f8c706005714c80d23a41ee1c5f596bf27e9051
KlementOndrej <ondrej.klement@unob.cz>
2022-12-30 14:16:41 +0100

    Code cleanup, memory leak fix

--- src/Game.hpp
+++ src/Game.hpp
@@ -12,8 +12,61 @@ public:
 		board = Board();
 		game_over = false;
 		srand(time(0));
+	}
 
-		//testspace
+	~Game(){
+		delete point;
+	}
+
+	void input(){
+		chtype ipt = board.getInput();
+
+		switch(ipt){
+		//move up
+		case KEY_UP:
+		case 'w':
+			snake.setDirection(up);
+			break;
+		//move down
+		case KEY_DOWN:
+		case 's':
+			snake.setDirection(down);
+			break;
+		//move right
+		case KEY_RIGHT:
+		case 'd':
+			snake.setDirection(right);
+			break;
+		//move left
+		case KEY_LEFT:
+		case 'a':
+			snake.setDirection(left);
+			break;
+		//pause
+		case 'p':
+			board.pause();
+			while(board.getInput() != 'p');
+			board.resume();
+			break;
+		}
+	}
+
+	void update(){
+		placePoint();
+
+		moveSnake();
+	}
+
+	void redraw(){
+		board.refresh();
+	}
+
+	bool isOver(){
+		return game_over;
+	}
+
+	//places snake and a point in upper left corner
+	void placeSnake(){
 		snake.setDirection(right);
 
 		SnakePiece next = SnakePiece(1, 1);
@@ -30,71 +83,27 @@ public:
 		board.add(next);
 		snake.addPiece(next);
 
-		point = new Point(2, 4);
+		point = new Point(next.getX(), next.getY()+2);
 		board.add(*point);
 
-		//testspace end
 	}
 
-	~Game(){
-		delete point;
-	}
-
-	void input(){
-		chtype ipt = board.getInput();
-
-		switch(ipt){
-		case KEY_UP:
-		case 'w':
-			snake.setDirection(up);
-			break;
-		case KEY_DOWN:
-		case 's':
-			snake.setDirection(down);
-			break;
-		case KEY_RIGHT:
-		case 'd':
-			snake.setDirection(right);
-			break;
-		case KEY_LEFT:
-		case 'a':
-			snake.setDirection(left);
-			break;
-		case 'p':
-			board.pause();
-			while(board.getInput() != 'p');
-			board.resume();
-			break;
-		}
-	}
-
-	void update(){
-		placePoint();
-
-		//test space
+	void moveSnake(){
 		SnakePiece next = snake.nextPiece();
-		if(!(next.getX() == point->getX() && next.getY() == point->getY())){
+		if(!(next.getX() == point->getX() && next.getY() == point->getY())){	//removes tail piece if next position does not have a point
 			board.add(Empty(snake.tail().getX(), snake.tail().getY()));
 			snake.removePiece();
 		}
 		else{
-			removePoint();
+			removePoint();	//snake eats point
 		}
-		board.add(next);
-		snake.addPiece(next);
-		//test space end
-	}
-
-	void redraw(){
-		board.refresh();
-	}
-
-	bool isOver(){
-		return game_over;
+		board.add(next);	//snake moves to next position
+		snake.addPiece(next);	//adds point on board if there is none
 	}
 
 	void removePoint(){
 			board.add(Empty(point->getX(), point->getY()));
+			delete point;
 			point = NULL;
 	}
 
